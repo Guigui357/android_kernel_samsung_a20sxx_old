f25c1fdc59443c98620c5cdc8aaee90d48de182b
Guigui357 <zebuchinho77@gmail.com>
2026-03-16 15:26:49 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,10 +5,12 @@ mkdir out
 export ARCH=arm64
 
 export CROSS_COMPILE=aarch64-linux-gnu-
-scripts/config --disable CC_STACKPROTECTOR_STRONG
-make olddefconfig
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android a20s_eur_open_defconfig
+scripts/config --disable CC_STACKPROTECTOR_STRONG
+scripts/config --disable CC_STACKPROTECTOR
+scripts/config --disable CC_STACKPROTECTOR_REGULAR
+make olddefconfig
 make -j16 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
