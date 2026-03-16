0a29a4577c7f5cb01617a3addc46154e5223d09a
Guigui357 <zebuchinho77@gmail.com>
2026-03-16 14:57:30 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,8 +4,7 @@ mkdir out
 
 export ARCH=arm64
 
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-
+export CROSS_COMPILE=aarch64-linux-gnu-
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android a20s_eur_open_defconfig
 make -j16 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y
  
